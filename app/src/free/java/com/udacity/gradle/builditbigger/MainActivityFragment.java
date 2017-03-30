91712a5e11b6c52d5aa0dfa179984493411555e9
“Neha <neha.shah@starzplayarabia.com>
2017-03-30 16:26:48 +0400

    Product flavors paid and Free!

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -14,6 +14,7 @@ import com.google.android.gms.ads.AdView;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    
 
     public MainActivityFragment() {
     }
